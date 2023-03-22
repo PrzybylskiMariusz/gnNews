1ea58a2a4ef6434bd5e60f581fd997ccfdf27175
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2023-03-22 13:54:32 +0100

    Create global styles for app

---
+++ src/theme/GlobalStyles.jsx
@@ -0,0 +1,26 @@
+import { createGlobalStyle, css } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle(
+	({ theme }) => css`
+		html {
+			box-sizing: border-box;
+			-webkit-font-smoothing: antialiased;
+			-moz-osx-font-smoothing: grayscale;
+			font-size: 62.5%;
+		}
+		*,
+		*:before,
+		*:after {
+			box-sizing: inherit;
+			margin: 0;
+			padding: 0;
+			font-family: ${theme.fonts.primary}, sans-serif;
+		}
+		body {
+			background-color: ${theme.colors.cream};
+			color: ${theme.colors.darkBlue};
+			font-size: 1.6rem;
+			font-family: inherit;
+		}
+	`
+);
---
+++ src/theme/theme.jsx
@@ -0,0 +1,18 @@
+export const theme = {
+	colors: {
+		darkCyan: "hsl(158, 36%, 37%)",
+		cream: "hsl(30, 38%, 92%)",
+		darkBlue: "hsl(212, 21%, 14%)",
+		grayishBlue: "hsl(228, 12%, 48%)",
+		white: "hsl(0, 0%, 100%)",
+	},
+	fonts: {
+		primary: "Oswald",
+		secondary: "Source Serif Pro",
+		weights: {
+			light: 300,
+			medium: 500,
+			bold: 600,
+		},
+	},
+};
