22e515d2edd7967e111adfba4003023899ff6ccf
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2023-03-23 12:38:28 +0100

    Handle display articles for choosen country

--- src/routes/Country.jsx
+++ src/routes/Country.jsx
@@ -1,5 +1,41 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { API_CODES, API_COUNTRY_NEWS, API_KEY } from "../api/api";
 import { StyledMainTemplate } from "../templates";
 
 export const Country = () => {
-	return <StyledMainTemplate>Country news</StyledMainTemplate>;
+	const { name } = useParams();
+	const [code, setCode] = useState("");
+	const [countryNews, setCountryNews] = useState([]);
+
+	const getCountryNews = async () => {
+		let countryCode;
+		try {
+			const { data } = await API_CODES.get(`${name}?fields=alpha2Code`);
+			const code = data[0].alpha2Code.toLowerCase();
+			countryCode = code;
+			const getCodes = async () => {
+				const { data } = await API_COUNTRY_NEWS.get(
+					`top-headlines?country=${countryCode}&apiKey=${API_KEY}`
+				);
+				setCountryNews(data);
+			};
+			getCodes();
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		getCountryNews();
+	}, [name]);
+
+	return (
+		<StyledMainTemplate>
+			{countryNews["articles"] &&
+				countryNews["articles"].map((article) => (
+					<div key={crypto.randomUUID()}>{article?.title}</div>
+				))}
+		</StyledMainTemplate>
+	);
 };
